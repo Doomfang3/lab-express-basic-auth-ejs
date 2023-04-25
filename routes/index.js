0111b93ef4aa5ab432294cdb4c68a6369c679636
ahmed awad <ahmeedsauce7@gmail.com>
2023-04-25 21:42:16 +0200

    iteration 1 and half 2 done

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
+const User = require("../models/User.model");
+
 const router = require("express").Router();
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
+});
+
+router.get("/application", (req, res, next) => {
+  res.render("application");
 });
 
 module.exports = router;
